0b48f60315ae31949b6895d624ca5a9a3a6d0166
pprimee <hphan13@masonlive.gmu.edu>
2017-04-07 14:46:51 -0400

    Add files via upload

---
+++ gameofchats/gameofchats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gameofchats
+//
+//  Created by Brian Voong on 6/24/16.
+//  Copyright © 2016 letsbuildthatapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+    
+    func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+
+}
+
